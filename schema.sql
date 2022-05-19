9cff418cdf319c5ff905d218fd9be1df27024e82
Jack <jclarkeparker@gmail.com>
2022-05-19 12:46:54 +1200

    Included a country_id column for the locations table in the schema

--- schema.sql
+++ schema.sql
@@ -10,7 +10,8 @@ CREATE TABLE countries(
 
 CREATE TABLE locations(
     id serial PRIMARY KEY,
-    name text NOT NULL
+    name text NOT NULL,
+    country_id integer NOT NULL REFERENCES countries(id) ON DELETE CASCADE
 );
 
 CREATE TABLE countries_journeys(
